4311170a65b3faf705b0cef66d3423c421950a04
Your Name <you@example.com>
2022-06-08 22:28:48 +0500

    day 3 js

--- jsConcepts/Js.js
+++ jsConcepts/Js.js
@@ -1,21 +1,26 @@
-// async function pending() {
-//   let response = await fetch("https://api.github.com/users");
-//   if (response.ok) {
-//     let json = await response.json();
-//     console.log("working");
-//     //  console.log(json[0]);
-//   }
-// }
-// pending();
+var userss = [];
 
-let promise = new Promise(async function (resolve, reject) {
-  resolve = await fetch("https://api.github.com/users");
-  if (resolve) {
-    let jsons = await resolve.json();
-    console.log("working");
-    console.log(jsons[0]);
-  } else {
-    reject = "error in loading";
-    console.log(reject);
-  }
+url = "https://api.github.com/users?per_page=10";
+async function fetchMoviesJSON() {
+  const response = await fetch(url);
+  userss = await response.json();
+  return userss;
+}
+fetchMoviesJSON().then((userss) => {
+  userss; // fetched movies
 });
+setTimeout(() => {
+  console.log(userss);
+  document.getElementById("1").innerHTML = userss
+    .map(
+      (user) =>
+        `<tr>
+      <th> ${user.login}</th>
+      <th> ${user.id}</th>
+      <th> ${user.node_id}</th>
+      <th> ${user.avatar_url}</th>
+      <th> ${user.gists_url}</th>
+    </tr>`
+    )
+    .join("");
+}, 4000);
