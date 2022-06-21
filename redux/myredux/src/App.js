e02a26f169e2a380e9c50873fe0ed40daf22f9a8
Your Name <you@example.com>
2022-06-21 20:11:08 +0500

    redux working

---
+++ redux/myredux/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Navbar from "./navbar";
+import Shop from "./shop";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Shop />
+    </>
+  );
+}
+
+export default App;
---
+++ redux/myredux/src/shop.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+const Shop = () => {
+  return (
+    <>
+      <button className="btn btn-primary m-2">+</button>
+      updateBalance
+      <button className="btn btn-primary m-2">-</button>
+    </>
+  );
+};
+
+export default Shop;
