da9613f5a4900d83bea8553108ea0997eb795adf
Ahmed Mujtaba <hafiz.ahmad101@gmail.com>
2022-06-24 17:20:49 +0500

    formik

--- forms/formik/src/App.js
+++ forms/formik/src/App.js
@@ -1,21 +1,14 @@
-import logo from "./logo.svg";
+import React, { Component } from "react";
 import "./App.css";
 import Basic from "./formBasix";
 import DATA from "./data";
-
-function App() {
-  const happen = false;
-  const navigate = () => {
-    if (happen) {
-      <DATA />;
-    }
-  };
-  return (
-    <div className="App">
-      <Basic />
-      <button className="btn" onClick={navigate}></button>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Basic />
+      </div>
+    );
+  }
 }
-
 export default App;
